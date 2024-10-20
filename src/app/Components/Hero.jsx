2890bc687cd3b05549dec676ba54380a75ae8672
fares mohamed <fm883254@gmail.com>
2024-10-21 01:54:38 +0300

    add sidebar

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 p-5 overflow-hidden ">
       <div className="p-5 flex flex-col justify-center">
-        <h2 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold mt-10 md:mt-20 leading-tight">
+        <h2 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold mt-10 md:mt-20 leading-tight tx_lighter" >
           Find The Best Fashion Style For You
         </h2>
         <p className="text-lg md:text-xl font-normal mt-5 md:mt-10 w-full md:w-[80%]">
@@ -16,7 +16,9 @@ const Hero = () => {
         </button>
       </div>
       <div className="flex items-center justify-center p-10 md:p-20">
-        <div className="relative w-full h-[300px] md:h-[500px] rounded-lg rounded-bl-[150px] overflow-hidden shadow-lg">
+        <div className="relative w-full h-[300px] md:h-[500px] rounded-lg rounded-bl-[150px] overflow-hidden"style={{
+          boxShadow:'5px 15px 25px #34251f'
+        }}>
           <Image src={'/images/main.jpg'} fill alt="main" className="object-cover" />
         </div>
       </div>
--- src/app/Components/Nav.jsx
+++ src/app/Components/Nav.jsx
@@ -25,12 +25,26 @@ export default function nav() {
     }));
     setNavigation(updatedNavigation);
   };
-  const { isLoged } = useContext(UserContext);
+  const { isLoged,setIsLoged } = useContext(UserContext);
+  const [view, setView] = useState(false)
+  const handleViewbar=()=>{
+    (view)?setView(false):setView(true);
+  }
+  const handleOut=(e)=>{
+    e.preventDefault();
+    setIsLoged(false);
+    setView(false)
+
+  }
   return (
     <Disclosure as="nav" className=" pt-4 sticky top-0 bg-myBlack z-50 bg-my_light text-black ">
       <div className="mx-auto w-full px-2 sm:px-6 lg:px-8 ">
         <div className="relative flex h-16 items-center justify-between ">
           <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+            {
+              isLoged ?
+                <Image onClick={handleViewbar} src={'/svgs/account.svg'}  alt="account" width={35} height={35} className="cursor-pointer sm:hidden block z-20" /> : ''
+            }
             {/* Mobile menu button*/}
             <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-black   focus:outline-none  focus:ring-none ">
               <span className="absolute -inset-0.5" />
@@ -44,11 +58,12 @@ export default function nav() {
                 className="hidden h-8 w-8 group-data-[open]:block border-2 border-black"
               />
             </DisclosureButton>
+
           </div>
           <div className="flex flex-1 items-center justify-start sm:items-stretch sm:justify-start px-8 sm:px-0">
             <div className="flex flex-shrink-0 items-center sm:">
-              <h1 className="uppercase text-black font-bold md:text-4xl text-base">
-                Eleanor <span className="text-myGreen">.</span>
+              <h1 className="uppercase text-black font-bold md:text-4xl text-base tx_lighter">
+                Eleanor.
               </h1>
             </div>
             <div className="hidden  sm:block ml-auto mr-0">
@@ -63,7 +78,7 @@ export default function nav() {
                       item.current
                         ? " text-myGreen font-bold underline underline-offset-8"
                         : " text-black",
-                      "block  px-3 py-2 text-2xl font-bold "
+                      "block  px-3 py-2 text-2xl font-bold  "
                     )}
                   >
                     {item.name}
@@ -71,9 +86,9 @@ export default function nav() {
                 ))}
                 {
                   isLoged ?
-                  <Image src={'/svgs/account.svg'} alt="account" width={30} height={30} className="cursor-pointer" />
-                  :
-                  <Link href={'/Log'} className=" flex items-center justify-center px-6  text-2xl font-normal border border-black uppercase">login</Link>
+                    <Image src={'/svgs/account.svg'} onClick={handleViewbar} alt="account" width={30} height={30} className="cursor-pointer" />
+                    :
+                    <Link href={'/Log'} className=" flex items-center justify-center px-6  text-2xl font-normal border border-black uppercase">login</Link>
                 }
               </div>
             </div>
@@ -98,14 +113,21 @@ export default function nav() {
           ))}
           {
             isLoged ?
-            <Link href={'/'} className="flex items-center justify-center  text-md font-bold ">Account</Link>
-            
-            : 
-            <Link href={'/Log'} className="flex items-center justify-center px-4  text-md font-normal border border-black uppercase">login</Link>
+              ''
+              :
+              <Link href={'/Log'}  className="flex items-center justify-center px-4  text-md font-normal border border-black uppercase">login</Link>
           }
-
         </div>
       </DisclosurePanel>
+      <div 
+      className={`account-panal absolute top-[80px] transition-all  ${(view)?`right-0`:`-right-full`} h-[calc(100vh-80px)] w-[200px] bg-gradient-to-tr
+       from-[#7d6241] to-[#dcb68c] rounded-tl-[30px] border-2 border-my_dark drp_sh p-4`}>
+        <Image src={'/svgs/close.svg'} onClick={handleViewbar} alt="close" width={35} height={35} className="cursor-pointer"/>
+        <div className="content flex flex-col items-start justify-center gap-5 mt-10 font-bold ">
+          <div className="text-xl self-center">Hi fares !</div>
+          <button className="bg-my_dark self-center text-white font-bold text-sm px-10 py-2 rounded-md uppercase my_shadow" onClick={handleOut}>log out</button>
+        </div>
+      </div>
     </Disclosure>
   );
 }
